8bcb3488aec9769d7cdd61f9c142ca9e598cdf27
andreaparisi <andrea@androidintheworld.it>
2014-09-12 01:15:27 +0200

    update

--- js/index.js
+++ js/index.js
@@ -87,6 +87,36 @@ var app = {
 					}
 				});
 
+{
+
+
+navigator.tts.startup(startupWin, fail);
+function startupWin(result) {
+    console.log("Startup win");
+    // When result is equal to STARTED we are ready to play
+    console.log("Result "+result);
+    //TTS.STARTED==2 use this once so is answered
+    if (result == 2) {
+        navigator.tts.getLanguage(win, fail);
+        navigator.tts.speak("The text to speech service is ready");                                     
+    }
+}                               
+
+function win(result) {
+    console.log(result);
+}
+
+function fail(result) {
+    console.log("Error = " + result);
+}
+
+ }
+
+
+
+
+
+
 
 
 
